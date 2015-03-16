40e8ab49814c40e402fcde8f2aca4f944d65cd4b
k8si <ksilvers@cs.umass.edu>
2015-03-16 17:45:28 -0400

    fix script

--- bin/train-conll-ner.sh
+++ bin/train-conll-ner.sh
@@ -3,10 +3,10 @@
 memory="15g"
 
 train="--train=data/conll2003/eng.train"
-dev="--dev=data/conll2003/eng.testa"
-test="--test=data/conll2003/eng.testb"
+#dev="--dev=data/conll2003/eng.testa"
+test="--test=data/conll2003/eng.testa"
 brown="--brown=data/brownBllipClusters"
 
-CP="target/nertools-0.1-SNAPSHOT.jar"
+CP="target/nertools-1.0-SNAPSHOT.jar":$FACJAR
 
-java -cp $CP -xMx$memory "edu.umass.cs.iesl.nertools.ConllNerMain" $train $dev $test $brown
+java -cp $CP -Xmx$memory "edu.umass.cs.iesl.nertools.ConllNerMain" $train $test $brown
